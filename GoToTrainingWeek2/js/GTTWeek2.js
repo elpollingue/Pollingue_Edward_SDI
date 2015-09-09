dfd15d9cb142f036bd93d3c091269af27b95a60f
elpollingue <edpoll83@gmail.com>
2015-09-09 10:39:57 -0500

    calculator and alert complete
    Variable ‘currentYear’ defined as 2015; variable ‘age’ defined as
    ‘currentYear-birthYear’; output alert prompts user with name and
    calculated age

--- GoToTrainingWeek2/js/GTTWeek2.js
+++ GoToTrainingWeek2/js/GTTWeek2.js
@@ -7,12 +7,28 @@
 
 //// Age Calculator
 
+        /// Set Variables
 
 // Prompt for user's name
 var name = prompt("Welcome to the Age Calculator!\nPlease enter your name:");
 
-
 // Use 'name' variable to create a personalized greeting
 alert("Welcome "+name+"!");
 
+// Ascertain user's birth year
+var birthYear = prompt("What is your birth year?");
 
+// Output 'birthYear' to console.log
+console.log(birthYear);
+
+// Set current year
+var currentYear = 2015;
+
+
+        // Calculate Age
+
+// Calculate age
+var age = currentYear-birthYear;
+
+// Return calculated age
+alert(name+", your current age is calculated to be "+age+".");
