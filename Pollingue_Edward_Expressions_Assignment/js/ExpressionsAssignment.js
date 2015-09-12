50b6b44ca30d65b2ea58aba72c8bbed799cf855e
elpollingue <edpoll83@gmail.com>
2015-09-11 19:16:23 -0500

    Expressions Assignment - further variables defined;
    defined variables concerning water consumption; opted to use variables
    rather than constants for water consumption; continuing coding

--- Pollingue_Edward_Expressions_Assignment/js/ExpressionsAssignment.js
+++ Pollingue_Edward_Expressions_Assignment/js/ExpressionsAssignment.js
@@ -8,12 +8,17 @@
 ////    will be used to determine water consumption rates, and this information will be presented to the user.
 
 
-//  Declare and Define constants
-
-const showerConsumption   = 15;
-const bathConsumption     = 25;
+///  Inform user of the purpose of the calculator (in order to clarify the purpose of future prompts).
+alert("This calculator will provide information concerning your household's water consumption.");
 
 
-// Prompt for user input and define variables
+///  Prompt for user input and define variables
+// prompt for the number of showers per day and cast the returned value as a number.
+var showersPerDay       =   Number(prompt("How many showers are taken per day?"));
+// prompt for the number of baths per day and cast the returned value as a number.
+var bathsPerDay         =   Number(prompt("How many baths are taken per day?"));
+// prompt for the average amount of water consumed per shower; cast returned value as a number.
+var showerConsumption   =   Number(prompt("On average, how much water is used per shower?"));
+// prompt for the average amount of water consumed per bath; cast returned value as a number.
+var bathConsumption     =   Number(prompt("On average, how much water is used per bath?"));
 
-var showersPerDay
