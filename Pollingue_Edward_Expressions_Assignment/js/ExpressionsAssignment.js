2d1556bed20a6e6491238fa8aed8bb23a61d1d38
elpollingue <edpoll83@gmail.com>
2015-09-11 19:03:27 -0500

    Expressions Assignment - Initial coding begun;
    calculator function determined; constants declared; continuing with
    variables

--- Pollingue_Edward_Expressions_Assignment/js/ExpressionsAssignment.js
+++ Pollingue_Edward_Expressions_Assignment/js/ExpressionsAssignment.js
@@ -1,5 +1,19 @@
 /*
  * Edward Pollingue		    SDI Online, Section 02
- * September 09, 2015       Activity: Go To Training Week 2
+ * September 11, 2015       Activity: Go To Training Week 2
  */
 
+
+////    This calculator will prompt for values concerning the water consumption habits of a household; the values returned
+////    will be used to determine water consumption rates, and this information will be presented to the user.
+
+
+//  Declare and Define constants
+
+const showerConsumption   = 15;
+const bathConsumption     = 25;
+
+
+// Prompt for user input and define variables
+
+var showersPerDay
