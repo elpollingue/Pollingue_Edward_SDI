8dd354d43be30f613b5ebe8b3de7f3b0a93b2ccd
elpollingue <edpoll83@gmail.com>
2015-09-20 07:36:43 -0500

    Conditionals Assignment; Initial Commit
    Folders and files created; beginning to define variables and validate
    input

---
+++ Pollingue_Edward_Conditionals_Assignment/js/Conditionals_Assignment.js
@@ -0,0 +1,14 @@
+/*
+ * Edward Pollingue		    SDI Online, Section 02
+ * September 20, 2015       Activity: Conditionals Assignment
+ */
+
+
+////    This script will serve as a calculator that determines the capacity of a forklift based on properties of
+////    the items that it is tasked with transporting, as determined by user input.
+
+
+///     Ascertain user input for each relevant measurement; validate each input, and re-prompt if input was invalid.
+
+var liftMaxVolume   = Number(promtp("What is the maximum volume "))
+var unitVolume
