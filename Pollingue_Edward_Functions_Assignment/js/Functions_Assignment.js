838037e4ed2c50eda6894b8c478fc9eb4f6b7427
elpollingue <edpoll83@gmail.com>
2015-09-24 09:23:01 -0500

    Functions: calculator function determined,
    initial template for validated prompt created

--- Pollingue_Edward_Functions_Assignment/js/Functions_Assignment.js
+++ Pollingue_Edward_Functions_Assignment/js/Functions_Assignment.js
@@ -4,3 +4,13 @@
  */
 
 
+////        This calculator will return information regarding the air speed velocity of a coconut laden swallow.
+
+///  Define variables: prompt for, validate and cast user input.
+
+var coconutMass         =   Number(prompt("What is the weight (in ounces) of the coconut being transported?"));
+while (!coconutMass){var coconutMass = Number(prompt("A numeric entry is required.\nWhat is the weight (in ounces) of the coconut being transported?"));}
+
+
+console.log(coconutMass);
+
